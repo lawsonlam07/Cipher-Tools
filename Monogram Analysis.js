227f54a89e176e24d00efc026a829a7ccefd6961
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-11-02 21:06:20 +0000

    Update Monogram Analysis.js

--- Monogram Analysis.js
+++ Monogram Analysis.js
@@ -1,6 +1,5 @@
 occurences = new Array(26).fill(0);
 percentages = [];
-
 const distribution = [
   8.55, 1.60, 3.16, 3.87, 12.10, 2.18, 2.09, 4.96, 7.33, 0.22, 0.81, 4.21, 2.53,
   7.17, 7.47, 2.07, 0.10, 6.33, 6.73, 8.94, 2.68, 1.06, 1.83, 0.19, 1.72, 0.11
@@ -27,7 +26,8 @@ function drawBars() {
 function getMousePos(x, y) {
   let letter = Math.floor(x/(windowWidth/26));
   let percent = Number.isNaN(percentages[letter]) && "0" || percentages[letter];
-  let info = `${String.fromCharCode(letter+65)}: ${occurences[letter]}\n${percent}%\n(${distribution[letter]}%)`;
+  let monogram = String.fromCharCode(letter+65);
+  let info = `${monogram}: ${occurences[letter]}\n${percent}%\n(${distribution[letter]}%)`;
   fill(100, 50);
   rect(letter*(windowWidth/26), -5, windowWidth/26, windowHeight);
   fill(25);
@@ -53,7 +53,7 @@ function drawLines() {
   line(0, windowHeight*(9/10), windowWidth, windowHeight*(9/10));
   for (let i = 10; i <= 110; i += 10) {
     stroke(0, 50);
-    let yPos = windowHeight*(9/10) - (maxBarHeight*(i/100))
+    let yPos = windowHeight*(9/10) - (maxBarHeight*(i/100));
     line(0, yPos, windowWidth, yPos);
     stroke(0, 0);
     text(i + "%", 0, windowHeight*(9/10) - (maxBarHeight*(i/100)));
@@ -70,7 +70,7 @@ function draw() {
   createCanvas(windowWidth, windowHeight-10);
   textSize(windowWidth/75);
   barWidth = windowWidth/79;
-  let maxPercent = Number.isNaN(Math.max(...percentages)) && 1 || Math.max(...percentages)
+  let maxPercent = Number.isNaN(Math.max(...percentages)) && 1 || Math.max(...percentages);
   maxBarHeight = windowHeight*(85/Math.max(15, maxPercent));
 
   updateCipher();
