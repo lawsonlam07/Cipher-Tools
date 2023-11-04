24397176b9f6edf3ca2770d92327ab9b502ffa8a
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-11-04 12:17:07 +0000

    Update Caesar Cipher.js

--- Caesar Cipher.js
+++ Caesar Cipher.js
@@ -3,6 +3,29 @@ const distribution = [
   7.17, 7.47, 2.07, 0.10, 6.33, 6.73, 8.94, 2.68, 1.06, 1.83, 0.19, 1.72, 0.11
 ];
 let plaintext = [];
+let shiftNo = 0
+
+function updateShiftNo(sign) {
+  if (sign) {
+    shiftNo++;
+    if (shiftNo > 25) {
+      shiftNo = 0;
+    }
+  } else {
+    shiftNo--;
+    if (shiftNo < 0) {
+      shiftNo = 25;
+    }
+  }
+}
+
+function rightButton() {
+  updateShiftNo(true)
+}
+
+function leftButton() {
+  updateShiftNo(false)
+}
 
 function stats() {
   occurences = Array(26).fill(0);
@@ -29,14 +52,15 @@ function fitness() {
     percentages.push(percentages.shift());
     difference.push(total);
   }
+  return difference.indexOf(Math.min(...difference));
 }
 
-function solve() {
+function shift(pos) {
   plaintext = [];
   for (let i = 0; i < ciphertext.length; i++) {
     let char = ciphertext[i];
     if (65 <= char.charCodeAt(0) && char.charCodeAt(0) <= 90) {
-      let newChar = char.charCodeAt(0) - difference.indexOf(Math.min(...difference));
+      let newChar = char.charCodeAt(0) - pos;
       plaintext.push(String.fromCharCode(newChar < 65 ? newChar + 26 : newChar));
     } else {
       plaintext.push(char);
@@ -44,19 +68,28 @@ function solve() {
   }
 }
 
-function solveCipher() {
-  textSize(50);
-  text("Ciphertext", 0, windowHeight*(1/7));
-  text("Plaintext", windowWidth*(51/100), windowHeight*(1/7));
+function drawUI() {
+  textSize(windowHeight/13.8);
+  text(shiftNo, windowWidth*(99/100)-windowHeight*(1.55/7), windowHeight*(5.65/6));
+  textStyle(BOLD);
+  text("Ciphertext", windowWidth/4, windowHeight*(1/7));
+  text("Plaintext", windowWidth*(3/4), windowHeight*(1/7));
+  textStyle(NORMAL);
+  line(1, 35, 1, windowHeight*(5/6));
+  line(windowWidth-11, 35, windowWidth-11, windowHeight*(5/6));
   line(0, windowHeight*(1/6), windowWidth, windowHeight*(1/6));
-  line(windowWidth/2, 0, windowWidth/2, windowHeight);
-  textSize(12);
-  text(input.value().toUpperCase(), windowWidth*(1/40), windowHeight*(1/5), windowWidth*(18/40), windowHeight*(3/4));
-  text(plaintext.join(""), windowWidth*(21/40), windowHeight*(1/5), windowWidth*(18/40), windowHeight*(3/4));
-  ciphertext = input.value().toUpperCase();
+  line(0, windowHeight*(5/6), windowWidth, windowHeight*(5/6));
+  line(0, 35, windowWidth, 35);
+  line(windowWidth/2, 35, windowWidth/2, windowHeight*(5/6));
+  textSize(windowHeight/57.5);
+  text(buttons.input.value().toUpperCase(), windowWidth*(1/40), windowHeight*(1/5), windowWidth*(18/40), windowHeight*(4.5/7));
+  text(plaintext.join(""), windowWidth*(21/40), windowHeight*(1/5), windowWidth*(18/40), windowHeight*(4.5/7));
+  ciphertext = buttons.input.value().toUpperCase();
+}
+
+function solveCipher() {
   stats();
-  fitness();
-  solve();
+  shiftNo = fitness();
 }
 
 function copyPlaintext() {
@@ -65,23 +98,58 @@ function copyPlaintext() {
   decrypt.value = plaintext.join("");
   decrypt.select();
   document.execCommand("copy");
-  if (decrypt.hasChildNodes()) {
-    document.body.removeChild();
+  decrypt.remove();
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case ENTER: copyPlaintext(); break;
+    case LEFT_ARROW: updateShiftNo(false); break;
+    case RIGHT_ARROW: updateShiftNo(true); break;
+    case 32: solveCipher(); break;
   }
 }
 
 function setup() {
-  createCanvas(windowWidth-25, windowHeight-10);
+  textAlign(CENTER);
   background(255);
-  input = createInput("");
-  input.position(windowWidth/100, windowHeight/100);
-  input.size = (windowWidth);
-  button = createButton("Copy");
-  button.position(input.x + input.width, input.y);
-  button.mousePressed(copyPlaintext);
+  buttons = {
+    input: createInput(),
+    copy: createButton("Copy"),
+    left: createButton("<"),
+    right: createButton(">"),
+    auto: createButton("Auto-Solve")
+  }
+  buttons.copy.mousePressed(copyPlaintext);
+  buttons.left.mousePressed(leftButton);
+  buttons.right.mousePressed(rightButton);
+  buttons.auto.mousePressed(solveCipher);
 }
 
 function draw() {
-  clear();
-  solveCipher();
+  createCanvas(windowWidth-10, windowHeight-10);
+  positions = {
+    input: [windowWidth/100, windowHeight/100],
+    copy: [windowWidth/100, windowHeight*(5.1/6)],
+    left: [windowWidth*(99/100)-windowHeight*(3/7), windowHeight*(5.1/6)],
+    right: [windowWidth*(99/100)-windowHeight*(1/7), windowHeight*(5.1/6)],
+    auto: [windowWidth/2 - windowHeight*(2/7), windowHeight*(5.1/6)]
+  }
+  sizes = {
+    input: [windowWidth*(97/100), 20],
+    copy: [windowHeight*(1.5/7), windowHeight*(1/7)],
+    left: [windowHeight*(1/7), windowHeight*(1/7)],
+    right: [windowHeight*(1/7), windowHeight*(1/7)],
+    auto: [windowHeight*(4/7), windowHeight*(1/7)]
+  }
+  for (let v in buttons) {
+    buttons[v].position(positions[v][0], positions[v][1]);
+    buttons[v].size(sizes[v][0], sizes[v][1]);
+    if (v != "input") {
+      buttons[v].style("font-size", `${windowHeight/13.8}px`);
+      buttons[v].style("border-radius", "10px");
+    }
+  }	
+  drawUI();
+  shift(shiftNo);
 }
