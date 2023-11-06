2583239a326264a430aa91a4cf5c0e097ba3791e
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-06 18:35:27 +0000

    Update Permutation Cipher.js

--- Permutation Cipher.js
+++ Permutation Cipher.js
@@ -1,1 +1,14 @@
+//This solver requires "trigrams.js" to analyse and give values to each trigram.
+function setup() {
+	input = createInput();
+	background(100);
+	console.log(trigrams["JQZ"]);
+}
 
+function draw() {
+	createCanvas(windowWidth-20, windowHeight-20);
+	input.position(windowWidth/100, windowWidth/100);
+	input.size(windowWidth*(97/100));
+	let plaintext = input.value().split(" ");
+	console.log(plaintext);
+}
