4f6edb6f8a33f20dab39c35ff215545185ecc6bd
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-20 17:36:51 +0000

    Update Polyalphabetic Vigenére Cipher Solver.js

--- Polyalphabetic Vigenére Cipher Solver.js
+++ Polyalphabetic Vigenére Cipher Solver.js
@@ -1,1 +1,12 @@
+/* Python solver!
+alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+ciphertext = [v for v in ciphertext if v in alpha]
 
+plaintext = []
+for i, v in enumerate(ciphertext):
+  newChar = ord(v) - ord(key[i % len(key)])
+  newChar %= 26
+  plaintext.append(alpha[newChar])
+
+print("".join(plaintext))
+*/
