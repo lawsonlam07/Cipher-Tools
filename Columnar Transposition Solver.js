6845f31ba3eeff0b0830d254bcc525d456297f72
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-11-25 10:47:14 +0000

    Update Columnar Transposition Solver.js

--- Columnar Transposition Solver.js
+++ Columnar Transposition Solver.js
@@ -1,1 +1,38 @@
+/* Once again, python!
+from itertools import permutations
 
+alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+keyLen = 7
+ciphertext = [v for v in ciphertext if v in alpha]
+block = len(ciphertext)//keyLen
+
+def fitness(arr):
+  total = 0
+  for i in range(len(arr) - 2):
+    total += trigrams["".join(arr[i: i + 3])]
+  return total
+
+def step(key, columns):
+  plaintext = []
+  for i in range(block):
+    for j in key:
+      plaintext.append(columns[j][i])
+  return plaintext
+
+def solve():
+  columns = []
+  solutions = [[], []]
+  choices = [i for i in range(keyLen)]
+
+  for i in range(keyLen):
+    columns.append(ciphertext[block * i: block * (i + 1)])
+
+  for key in permutations(choices, keyLen):
+    plaintext = step(key, columns)
+    solutions[0].append(plaintext)
+    solutions[1].append(fitness(plaintext))
+
+  return solutions[0][solutions[1].index(max(solutions[1]))]
+
+print("".join(solve()))
+*/
